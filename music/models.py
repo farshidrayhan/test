e34cf1f6995626c86e040db88178da97b000419f
farshid <farshidrayhan57@gmail.com>
2017-06-17 04:08:07 +0600

    new

--- music/models.py
+++ music/models.py
@@ -41,6 +41,8 @@ class Song(models.Model):
 class Drugs(models.Model):    #
     drug  = models.CharField(max_length=50)
     fingerprint  = models.CharField(max_length=900)
+    name  = models.CharField(max_length=150)
+
 
     def __str__(self):
-        return self.drug
+        return self.name
